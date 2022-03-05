8ffe32123147a6e84f749c7177e0e703523ed08d
FeverCode <giddylancs.gl@gmail.com>
2022-03-05 15:48:59 +0300

    git commit will add an array for day of week and akan names for male and females

--- script.js
+++ script.js
@@ -4,3 +4,17 @@
       return false;
     });
   });
+
+
+
+  var day =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi","Kwame"];
+  var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  function generateName(){
+
+
+
+
+
+  }
